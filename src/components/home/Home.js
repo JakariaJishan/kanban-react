df7a18dd88e0d1aa30bd25f315ebd7247653902e
JakariaJishan <jakariajishan987@gmail.com>
2023-07-28 11:06:09 +0600

    Create todo and task component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Task from '../task/Task'
+import Todo from '../todo/Todo'
 
 const Home = () => {
   return (
-    <div className='text-red-600'>Home</div>
+    <div className='flex justify-center items-center h-screen  bg-slate-600 text-white'>
+      <Todo/>
+      <Task/>
+    </div>
   )
 }
 
