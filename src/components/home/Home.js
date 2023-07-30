3e85121d0d2925a8559b9050625bbd2b605d9065
JakariaJishan <jakariajishan987@gmail.com>
2023-07-31 01:16:19 +0600

    Customize height of card

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,7 +4,7 @@ import DragDrop from '../drag&drop/DragDrop';
 
 function Home() {
   return (
-    <div className="bg-[#22272E] h-screen text-white">
+    <div className="">
       <DragDrop />
     </div>
   );
